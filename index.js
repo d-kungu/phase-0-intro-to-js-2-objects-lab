fcb16daf458a352c77527d92765308cd3b22f8ff
Andrew Kungu <drewskungu@gmail.com>
2025-03-24 13:15:36 +0300

    objects

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 // Write your solution in this file!
+let employee = {
+name: "Sam",
+streetAdress: "11 Broadway",
+
+};
+
+function updateEmployeeWithKeyAndValue(employee, key, value){
+    return { ...employee,
+        [key]: value
+
+    }
+} 
+
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee,key,value){
+
+        employee[key] = value 
+    return employee
+};
+
+function deleteFromEmployeeByKey(employee, key){
+    let newEmployee = {...employee};
+    delete newEmployee[key]
+    return newEmployee;
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee, key){
+delete employee[key]
+let newEmployee= employee
+return newEmployee
+     
+}
+
+
+
